a2cf4018c666d2907eb93566c71e32f9a81484a3
n0t-a-username <153406659+n0t-a-username@users.noreply.github.com>
2023-12-10 17:41:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-window.onresize = function() {
-    let container = document.getElementById("card");
-    const minScale = 1;
-    const maxScale = 2;
-    let scale = Math.min(window.innerWidth / (container.offsetWidth + 8),
-            window.innerHeight / (container.offsetHeight + 8));
-    scale = Math.min(maxScale, Math.max(minScale, scale))
-    document.documentElement.style.setProperty("--trickyScale", scale);
-}
+
